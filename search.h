82e59164a9200d87043d997d5ad0c19b86f18ec8
Diya Agnes <Diya-Agnes@users.noreply.github.com>
2017-11-13 16:08:06 +0300

    Update search.h

--- search.h
+++ search.h
@@ -1,4 +1,4 @@
-void search_empno()
+void searchbyno()
 {
 ifstream file;
 file.open("ademloyee.dat",ios::binary|ios::in);
@@ -8,15 +8,17 @@ cout<<"Enter the Employee number:";
 cin>>emp_no;
 while(file.read((char*)&E,sizeof(E)))
 {
-  if(emp_no==E.retempno())
+  if(emp_no==E.get_empno())
     {
-    s.output();
-    break;}}
+    E.output();
+    break;
+    }
+}
   
  file.close();
 }
 
-void search_name()
+void searchbyname()
 {   
   ifstream n;
   n.open("Emp.txt",ios::binary|ios::in);
@@ -36,7 +38,7 @@ void search_name()
     }
   }n.close();
 }
-void search_dept()
+void searchbydept()
 { 
 ifstream file;
 file.open("ademloyee.dat",ios::binary|ios::in);
@@ -58,32 +60,35 @@ file.open("ademloyee.dat",ios::binary|ios::in);
   void search()
   {
     clrscr();
-    int choice;
-    do{
-      cout<<"\n\n\n\tSearch\n";
-      cout<<"\t\t1.By Empno\n";
-      cout<<"\t\t2.By Name\n";
-      cout<<"\t\t3.By Department\n";
-      cout<<"\tEnter your choice\n";
-      cin>>choice;
+    int opt;
+    do {
+      cout<<"Search for an employee.";
+      cout<<"1.Employee number";
+      cout<<"2.Employee's name";
+      cout<<"3.Department";
+      cout<<"Kindly provide your option.(...The option must be the number!...):";
+      cin>>opt;
       
-      switch(choice)
-      {
-        case 1:search_empno();      
-               break;
+      
+              switch(opt)
+              {
+              case 1:searchbyempno();      
+                     break;
         
-        case 2:search_name();      
-               break;
+              case 2:searchbyname();      
+                     break;
         
-        case 3:search_dept();   
-               break;
+              case 3:searchbydept();   
+                     break;
          
-        default:cout<<"Sorry !!Invalid choice\n";
-                break;
+              default:cout<<"Your choice is invalid.";
+                      break;
+          
+               }
       
-      }
       cout<<"Press any key to continue......\n";
       getch();
       clrscr();
     
-      }while(choice!=4);}
+      }while(opt!=4);
+  }
