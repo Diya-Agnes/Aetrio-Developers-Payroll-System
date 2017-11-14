fde17a77bcef26f5c7082d759fa185e996ae25ac
Diya Agnes <Diya-Agnes@users.noreply.github.com>
2017-11-14 05:40:09 +0300

    Update salary-slip.h

--- salary-slip.h
+++ salary-slip.h
@@ -1,30 +1,81 @@
-void SalarySlip()
+void time()
+{
+ 
+
+//printing salary slip function
+void salary_slip_generator()
 {
  clrscr();
- int Eno, S=0;
+ 
+ int eno, S=0;
  Employee E;
- cout<<"Enter the employee number of the employee whose salary slip is to be displayed:";
- cin>>Eno;
- ifstream infile("Payroll.dat", ios::binary|ios::in);
- while(!infile.eof())
+ 
+ cout<<"We shall now display the salary slip requested for.";
+ cout<<"Employee number:";
+ 
+ cin>>eno;
+
+ ifstream file("ademployee.dat", ios::binary|ios::in);
+ while(!file.eof())
  {
-  infile.read((char*)&E, sizeof(E));
-  if(E.eno==Eno)
+  file.read((char*)&E, sizeof(E));
+  if(E.employeeno==eno)
   {
-   cout<<"\nSALARY SLIP\n";
-   cout<<"\nBasic Allowance:"<<E.basic;
-   cout<<"\nHuman Resource Allowance:"<<E.hra;
-   cout<<"\nDaily Allowance:"<<E.da;
-   cout<<"\nTransport Allowance:"<<E.transport;
-   cout<<"\nTax:"<<E.tax;
-   cout<<"\nNet Salary:"<<E.Total();
-   S=1;
+       gotoxy(28.5,3);
+       cout<<"SALARY SLIP";
+   
+       gotoxy(28.5,4);
+       cout<<"--------------";
+  
+   gotoxy(7,7);
+   cout<<"PERSONAL";
+   
+   gotoxy(6,8);
+   cout<<"-----------";
+   
+    cout<<"\nName:"<<E.name;
+    cout<<"\nAge:"<<E.age;
+    cout<<"\nDate Of Birth:"<<E.dd<<" | "<<E.mm<<" | "<<E.yy;
+    cout<<"\nTelephone Number:"<<E.telephoneno;
+  
+   gotoxy(60,7);
+   cout<<"OFFICIAL";
+   
+   gotoxy(6,8);
+   cout<<"-----------"; 
+  
+   gotoxy(55,9);
+   cout<<"Basic Salary:"<<E.basic;
+   gotoxy(55,10);
+   cout<<"\nHouse Rent Allowance:"<<E.hra;
+   gotoxy(55,11);
+   cout<<"\nDearness Allowance:"<<E.da;
+   gotoxy(55,12);
+   cout<<"\nTransport Allowance:"<<E.transport_a;
+   gotoxy(55,13);
+   cout<<"\nOvertime Allowance:"<<E.overtime_a;
+   gotoxy(55,14);
+   cout<<"\nDeductions:"<<E.deductions<<endl;
+   
+   cout<<'\n';
+   for(i=0;i<80;i++)
+   {
+    cout<<'_';
+   } 
+   
+   gotoxy(21.5,18);
+   cout<<"\nSalary(Gross):"<<E.salary;
+ 
+   found=1;
    break;
   }
+  
  }
- if(S==0)
-  cout<<"Employee not found!";
- infile.close();
- cout<<"Press any key to continue. ";
+ 
+ if(found==0)
+ cout<<"This employee's does not exist";
+ file.close();
+    
+ cout<<"ENTER key to continue. ";
  getch();
 }
