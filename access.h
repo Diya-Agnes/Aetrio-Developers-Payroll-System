176f99900875fda7261bb4ce1385eef6080852a8
Diya Agnes <Diya-Agnes@users.noreply.github.com>
2017-11-14 04:25:23 +0300

    Update access.h

--- access.h
+++ access.h
@@ -29,15 +29,52 @@ class USER
   long otpnumber[10];
   
   public:
-  
+	
+  USER
+  {
+     strcpy(usname,"AetrioD");
+  }
+	
+  char* return_username()
+  {
+    return usname;
+  }
+	
   char* return_otp()
   {
     return otp;
   }
-  
-
-  void login()
+     
+  char* generate_otp()
+  {  
+  char* otp[7];
+  for(n=0;n<6;n++)
   {
+  otp[n]=random(10);
+  cout<<otp[n];
+  }	  
+  }
+
+  void waitotp(int x)
+  {	  
+  for(int i=0;i<x;i++)
+  cout<<" ";
+  }	
+  }
+	
+  void eraseotp()
+  {
+   gotoxy(50,17);
+   for(i=0;i<=20;i++)
+   cout<<" "; 	
+  }
+
+  void login();
+}U;
+
+//Login function------------------------------------------------------------------------------------------------------------------------
+void USER::login()
+{
 
   gotoxy(30,10);
   cout<<"\nUsername:";
@@ -47,47 +84,26 @@ class USER
   gotoxy(30,13);
   cout<<"\nPassword:";
   
-  
   randomize();
   int n,i;
   char otp[7];
- 
-  
+	  
   gotoxy(50,17);
   cout<<"\nYour OTP is:";
   gotoxy(63,17);
   
-  generate_otp();
-  waitotp(5);
-  
-  gotoxy(50,17);
-  for(i=0;i<=20;i++)
-  cout<<" ";
-  
+    generate_otp();
+    waitotp(5);
+    eraseotp();
+
   gotoxy(40,13);
   cin>>otpnumber;
   
-  if(strcmp(otpnumber,return_otp())==0)
-  {
-   clrscr();
+    if(strcmp(usname,return_username())==0 && strcmp(otpnumber,return_otp())==0)
+    {
+     clrscr();
   
-   align_cntr("ACCESS GRANTED.");
-  }
-  
-   
-  char* generate_otp()
-  {
-  char* otp[7];
-  for(n=0;n<6;n++)
-  {
-  otp[n]=random(10);
-  cout<<otp[n];
-  }
-  }
-
-  void waitotp(int x)
-  {
-  for(int i=0;i<x;i++)
-  cout<<" ";
-  }
-  }
+     align_cntr("ACCESS GRANTED.");
+    }
+	
+}
